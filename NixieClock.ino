d1d9cbbb243c7a35d1671c7da0d0d262d3b65439
Sam Bach <spbach1234@gmail.com>
2020-03-02 16:43:02 -0500

    Added RTC battery dead alert with seperators, require time to be uploaded

--- NixieClock.ino
+++ NixieClock.ino
@@ -37,7 +37,27 @@ void setup() {
 
   // Check if RTC battery died
   if (rtc.lostPower()) {
-    Serial.println("RTC lost power, send time in format: YYYYMMDDHHMMSS");  // Request time to be sent in format
+    while (true) {
+      bool breakLoop = false;
+      while (Serial.available() > 0) {  // Check if anything has been received on the serial port, add it to the int buffer array
+        delay(10); // Buffer fill delay
+        byte bufferCount = Serial.available(); // byte with length of recieved bytes
+        int serBuffer[bufferCount]; // Generate array with said length
+        serBuffer[0] = Serial.read();  // Read the char value into the first array pos
+        for (int i = 1; i < bufferCount; i++) {  // Get all the other values as int
+          serBuffer[i] = (Serial.read() - '0');
+        }
+        configMode(serBuffer, bufferCount);  // Send buffer to the config function
+        breakLoop = true;
+        
+      }
+      digitalWrite(8, !(digitalRead(8)));
+      digitalWrite(9, !(digitalRead(9)));
+      delay(150);
+      if (breakLoop) {
+        break;
+      }
+    }
   }
 
   // Initialize direct port registers in output mode for ports A, C, and L
@@ -55,13 +75,13 @@ void setup() {
     for (int i = 0; i < 8; i++) {
       configParameters[i] = EEPROM.read(i); // Load current settings from EEPROM
     }
-    Serial.println("Settings Restored from EEPROM");  
+    Serial.println("Settings Restored from EEPROM");
   }
 
   if (configParameters[2] == 1) {  // If set to solid on mode, turn seperators on fixed
     digitalWrite(8, HIGH);
     digitalWrite(9, HIGH);
-  } else if (configParameters[2] == 0) {  // Keep pins from floating 
+  } else if (configParameters[2] == 0) {  // Keep pins from floating
     digitalWrite(8, LOW);
     digitalWrite(9, LOW);
   }
@@ -72,44 +92,44 @@ void loop() {
   DateTime now = rtc.now(); // Get current time from the RTC
 
   if (now.hour() > 12 && configParameters[1] == 0) {  // Compensates for time from RTC being 24 hour, unless in 24 hour mode
-      int hr = now.hour() - 12;
-      SetTube(1, nthdig(1, hr));
-      SetTube(2, nthdig(0, hr));
-    } else if (now.hour() == 0 && configParameters[1] == 0) {  // Changes 0 to 12 at midnight if not in 24 hour mode
-      SetTube(1, 1);
-      SetTube(2, 2);
-    } else {
-      SetTube(1, nthdig(1, now.hour()));  // Displays the current hour on the tubes
-      SetTube(2, nthdig(0, now.hour()));
+    int hr = now.hour() - 12;
+    SetTube(1, nthdig(1, hr));
+    SetTube(2, nthdig(0, hr));
+  } else if (now.hour() == 0 && configParameters[1] == 0) {  // Changes 0 to 12 at midnight if not in 24 hour mode
+    SetTube(1, 1);
+    SetTube(2, 2);
+  } else {
+    SetTube(1, nthdig(1, now.hour()));  // Displays the current hour on the tubes
+    SetTube(2, nthdig(0, now.hour()));
+  }
+
+  SetTube(3, nthdig(1, now.minute())); // Displays the current minute on the tubes
+  SetTube(4, nthdig(0, now.minute()));
+
+  SetTube(5, nthdig(1, now.second())); // Displays the current second on the tubes
+  SetTube(6, nthdig(0, now.second()));
+
+  if (configParameters[2] == 2) {  // Timing for toggling seperators.  Counts changes in milliseconds and bases timing on that
+    unsigned long currentMillis = millis();
+    if ((currentMillis - previousMillis) >= 1000) {
+      digitalWrite(8, !(digitalRead(8)));
+      digitalWrite(9, !(digitalRead(9)));
+      previousMillis = millis();
     }
+  }
 
-    SetTube(3, nthdig(1, now.minute())); // Displays the current minute on the tubes
-    SetTube(4, nthdig(0, now.minute()));
-
-    SetTube(5, nthdig(1, now.second())); // Displays the current second on the tubes
-    SetTube(6, nthdig(0, now.second()));
-
-    if (configParameters[2] == 2) {  // Timing for toggling seperators.  Counts changes in milliseconds and bases timing on that
-      unsigned long currentMillis = millis();
-      if ((currentMillis - previousMillis) >= 1000) {
-        digitalWrite(8, !(digitalRead(8)));
-        digitalWrite(9, !(digitalRead(9)));
-        previousMillis = millis();
-      }
+  while (Serial.available() > 0) {  // Check if anything has been received on the serial port, add it to the int buffer array
+    delay(10); // Buffer fill delay
+    byte bufferCount = Serial.available(); // byte with length of recieved bytes
+    int serBuffer[bufferCount]; // Generate array with said length
+    serBuffer[0] = Serial.read();  // Read the char value into the first array pos
+    for (int i = 1; i < bufferCount; i++) {  // Get all the other values as int
+      serBuffer[i] = (Serial.read() - '0');
     }
+    configMode(serBuffer, bufferCount);  // Send buffer to the config function
+  }
 
-    while (Serial.available() > 0) {  // Check if anything has been received on the serial port, add it to the int buffer array
-      delay(10); // Buffer fill delay
-      byte bufferCount = Serial.available(); // byte with length of recieved bytes
-      int serBuffer[bufferCount]; // Generate array with said length
-      serBuffer[0] = Serial.read();  // Read the char value into the first array pos
-      for (int i = 1; i < bufferCount; i++) {  // Get all the other values as int
-        serBuffer[i] = (Serial.read() - '0');
-      }
-      configMode(serBuffer, bufferCount);  // Send buffer to the config function
-    }
-
-    delay(10);  // Stability delay
+  delay(10);  // Stability delay
 }
 
 // Functions for main code:
@@ -169,9 +189,9 @@ void configMode(int *parameters, byte bufferCount) {  // Takes a pointer to the 
           indicatorMessage(1, 300);
         } else {
           break;
-        }  
+        }
       } else {
-         break;
+        break;
       }
       break;
     default:
